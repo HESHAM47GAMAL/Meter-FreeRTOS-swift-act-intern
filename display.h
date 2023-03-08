0f6e4a4a415239e303c01e34b4196c749ba3bd00
Etcho <hesham.shehatah00@eng-st.cu.edu.eg>
2023-03-08 06:09:31 +0200

    fist commit

---
+++ display.h
@@ -0,0 +1,13 @@
+#ifndef __DISPLAY_H__
+#define __DISPLAY_H__
+
+/* Define display task priority */
+/* Add your code here! */
+#define DISP_TASK_PRIORITY 1
+/* End of your code! */
+
+void DISP_Init(void);
+
+
+
+#endif
